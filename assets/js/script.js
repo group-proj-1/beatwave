f10a0653c227c3869f521b2a3cc147c6b9b83aa8
Valerie Michalecki <valerie227@gmail.com>
2021-05-08 13:21:59 -0500

    js progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+function getRandom(arr) {
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
 function getToken() {
     const clientId = '7b1ddde2b4bd4ee182d6a36abe2a35cd';
     const clientSecret = '7774875a0e4c46feb89f46acd70dc278';
@@ -22,12 +27,15 @@ function getToken() {
                 .then(response => response.json())
                 .then(data => {
                     let player = document.getElementById("songVariable");
+                    console.log(data.playlists);
 
-                    // create a playlist with POST method and userID with fetch
+                    let randomPlaylist = getRandom(data.playlists.items);
+                    let randomPlaylistId = randomPlaylist.id;
+                    console.log(randomPlaylistId);
                     // get that playlist ID and push 10 songs into the playlist with fetch (data.tracks.id) use forEach
                     // the playlist id is a variable
 
-                    player.src = `https://open.spotify.com/embed/playlist/${data.playlists.items[0].id}`
+                    player.src = `https://open.spotify.com/embed/playlist/${randomPlaylistId}`
                     console.log(data);
                 });
             console.log(data);
